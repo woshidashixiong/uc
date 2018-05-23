3fb8df5a74d13738316f3fb94349f631b1217d4d
Yang lu <yanglu@Yangs-MacBook-Pro-2.local>
2018-05-23 20:57:37 +0800

    initial push

---
+++ src/main/scala/common/util.scala
@@ -0,0 +1,70 @@
+package common
+
+import java.io.{File, FileInputStream}
+import java.util.Properties
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.hive.HiveContext
+
+object util {
+  // initial spark context
+  def spark_context(): SparkContext = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    val sc = new SparkContext("local[*]", "prod_selection")
+    sc
+  }
+
+  // load data from disk
+  def load_data_from_disk(sc: SparkContext, path: String): RDD[String] = {
+    val data = sc.textFile(path)
+    data
+  }
+
+  // load property config file
+  def load_config(): Properties = {
+    val pwd = new File(".").getCanonicalPath()
+    val path_file = "/src/main/scala/conf/rp.properties"
+    val path = pwd + path_file
+    val inputStream = new FileInputStream(path)
+    val config = new Properties()
+    config.load(inputStream)
+    config
+  }
+
+  // load config file
+  def get_config(variable : String): String = {
+    load_config().getProperty(variable)
+  }
+
+  // load spark session
+  def spark_session(): SparkSession = {
+    val config : Config = ConfigFactory.load()
+
+    val spark = SparkSession
+      .builder()
+      .enableHiveSupport()
+      .appName("hivemodes")
+      .config("spark.defalut.parallelism", config.getString("partitions.num"))
+      .config("spark.rdd.compress", "true")
+      .config("spark.locality.wait", "120")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .config("spark.kryoserializer.buffer.max", "1024")
+      .config("spark.sql.inMemoryColumnarStorage.batchSize", "10000")
+      .config("hive.metastore.uris", config.getString("hive.url"))
+      .enableHiveSupport()
+      .getOrCreate()
+    spark
+  }
+
+  // load hive context
+  def hive_context(ss: SparkSession): HiveContext ={
+
+    val hiveContext = new HiveContext(ss.sparkContext)
+    hiveContext
+  }
+
+}
